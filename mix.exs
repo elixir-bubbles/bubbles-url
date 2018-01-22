351e554cb4d893f58ab8533ecced163b2e1ac116
Martin Vrkljan <mvrkljan@gmail.com>
2018-01-23 00:57:18 +0100

    Update travis.yml commands

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Bubbles.Url.Mixfile do
 
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
